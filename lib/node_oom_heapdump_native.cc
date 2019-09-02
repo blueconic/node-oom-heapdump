9d38ff426324dd6b0848644b856f46733845600c
Paul Rütter <paul@blueconic.com>
2019-09-02 13:30:50 +0200

    Initial try-out for making it Node 12 compatible. Still we should look into using "Maybe" version of v8 API: https://github.com/joyeecheung/node/blob/v8-maybe-doc/CPP_STYLE_GUIDE.md#use-maybe-version-of-v8-apis

--- lib/node_oom_heapdump_native.cc
+++ lib/node_oom_heapdump_native.cc
@@ -2,10 +2,10 @@
 #include <v8-profiler.h>
 #include <stdlib.h>
 #if defined(_WIN32)
-  #include <time.h>
-  #define snprintf _snprintf
+#include <time.h>
+#define snprintf _snprintf
 #else
-  #include <sys/time.h>
+#include <sys/time.h>
 #endif
 
 using namespace v8;
@@ -13,51 +13,57 @@ using namespace v8;
 char filename[256];
 bool addTimestamp;
 
-class FileOutputStream: public OutputStream {
-  public:
-    FileOutputStream(FILE* stream): stream_(stream) { }
-    virtual int GetChunkSize() {
-      return 65536;
-    }
-    virtual void EndOfStream() { }
-    virtual WriteResult WriteAsciiChunk(char* data, int size) {
-      const size_t len = static_cast<size_t>(size);
-      size_t off = 0;
-      while (off < len && !feof(stream_) && !ferror(stream_))
-        off += fwrite(data + off, 1, len - off, stream_);
-      return off == len ? kContinue : kAbort;
-    }
+class FileOutputStream : public OutputStream
+{
+public:
+  FileOutputStream(FILE *stream) : stream_(stream) {}
+  virtual int GetChunkSize()
+  {
+    return 65536;
+  }
+  virtual void EndOfStream() {}
+  virtual WriteResult WriteAsciiChunk(char *data, int size)
+  {
+    const size_t len = static_cast<size_t>(size);
+    size_t off = 0;
+    while (off < len && !feof(stream_) && !ferror(stream_))
+      off += fwrite(data + off, 1, len - off, stream_);
+    return off == len ? kContinue : kAbort;
+  }
 
-  private:
-    FILE* stream_;
+private:
+  FILE *stream_;
 };
 
-void OnOOMError(const char *location, bool is_heap_oom) {
-  if (addTimestamp) {
+void OnOOMError(const char *location, bool is_heap_oom)
+{
+  if (addTimestamp)
+  {
     // Add timestamp to filename
     time_t rawtime;
-    struct tm* timeinfo;
+    struct tm *timeinfo;
     time(&rawtime);
     timeinfo = localtime(&rawtime);
 
-    char * pch;
-    pch = strstr (filename,".heapsnapshot");
-    strncpy (pch,"",1);
-    strcat (filename, "-%Y%m%dT%H%M%S.heapsnapshot");
-    
+    char *pch;
+    pch = strstr(filename, ".heapsnapshot");
+    strncpy(pch, "", 1);
+    strcat(filename, "-%Y%m%dT%H%M%S.heapsnapshot");
+
     char newFilename[256];
     strftime(newFilename, sizeof(filename), filename, timeinfo);
     strcpy(filename, newFilename);
   }
 
   fprintf(stderr, "Generating Heapdump to '%s' now...\n", filename);
-  FILE* fp = fopen(filename, "w");
-  if (fp == NULL) abort();
+  FILE *fp = fopen(filename, "w");
+  if (fp == NULL)
+    abort();
 
   // Create heapdump, depending on which Node.js version this can differ
   // for now, just support Node.js 7 and higher
-  const HeapSnapshot* snap = v8::Isolate::GetCurrent()->GetHeapProfiler()->TakeHeapSnapshot();
-  
+  const HeapSnapshot *snap = v8::Isolate::GetCurrent()->GetHeapProfiler()->TakeHeapSnapshot();
+
   FileOutputStream stream(fp);
   snap->Serialize(&stream, HeapSnapshot::kJSON);
   fclose(fp);
@@ -66,24 +72,32 @@ void OnOOMError(const char *location, bool is_heap_oom) {
   exit(1);
 }
 
-void ParseArgumentsAndSetErrorHandler(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
+void ParseArgumentsAndSetErrorHandler(const FunctionCallbackInfo<Value> &args)
+{
+  Isolate *isolate = args.GetIsolate();
   isolate->SetOOMErrorHandler(OnOOMError);
-  
-  // parse JS arguments
-  // 1: filename
-  // 2: addTimestamp boolean
-  #if NODE_VERSION_AT_LEAST(9, 0, 0)
-  String::Utf8Value fArg(isolate, args[0]->ToString());
-  #else
-  String::Utf8Value fArg(args[0]->ToString());
-  #endif
-  strncpy(filename, (const char*)(*fArg), sizeof(filename) - 1);
 
+// parse JS arguments
+// 1: filename
+// 2: addTimestamp boolean
+#if NODE_VERSION_AT_LEAST(12, 0, 0)
+  String::Utf8Value fArg(isolate, args[0]->ToString(isolate));
+#elif NODE_VERSION_AT_LEAST(9, 0, 0)
+  String::Utf8Value fArg(isolate, args[0]->ToString());
+#else
+  String::Utf8Value fArg(args[0]->ToString());
+#endif
+  strncpy(filename, (const char *)(*fArg), sizeof(filename) - 1);
+
+#if NODE_VERSION_AT_LEAST(12, 0, 0)
+  addTimestamp = args[1]->BooleanValue(isolate);
+#else
   addTimestamp = args[1]->BooleanValue();
+#endif
 }
 
-void init(Local<Object> exports) {
+void init(Local<Object> exports)
+{
   NODE_SET_METHOD(exports, "call", ParseArgumentsAndSetErrorHandler);
 }
 
